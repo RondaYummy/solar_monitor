537fe2376adf2f0b052c4c1e6c5647b1f32dd0a9
RondoYummy <basrers@ukr.net>
2024-12-21 01:49:43 +0200

    2

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger('Bootstrap');
+
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.disable('x-powered-by').disable('etag');
 
   await app.listen(3000);
-  console.log(`🟢 Application is running on: ${await app.getUrl()}`);
+  logger.log(`🟢 Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
