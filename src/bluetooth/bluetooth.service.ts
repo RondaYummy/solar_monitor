513b84e894a22600ac480ce9b03a2f86c8bff7db
RondoYummy <basrers@ukr.net>
2024-12-22 22:00:48 +0200

    2

--- src/bluetooth/bluetooth.service.ts
+++ src/bluetooth/bluetooth.service.ts
@@ -6,7 +6,7 @@ import { Buffer } from 'buffer';
 @Injectable()
 export class BluetoothService implements OnModuleInit {
   private readonly logger = new Logger(BluetoothService.name);
-  private connectedDevice: noble.Peripheral | null = null;
+  private connectedDevices: Map<string, noble.Peripheral> = new Map();
 
   async onModuleInit() {
     this.logger.log('Initializing Bluetooth... Current state: ' + noble?._state);
@@ -14,14 +14,21 @@ export class BluetoothService implements OnModuleInit {
     noble.on('discover', async (peripheral) => {
       const manufacturerData =
         peripheral.advertisement.manufacturerData?.toString('hex');
+      const localName = peripheral.advertisement.localName;
 
       // Якщо це потрібний вам пристрій (перевіряємо за виробником або назвою)
       if (
         config.allowedDevices.includes(manufacturerData) ||
-        config.allowedDevices.includes(peripheral.advertisement.localName)
+        config.allowedDevices.includes(localName)
       ) {
+        const deviceId = localName || manufacturerData || peripheral.address;
+        if (this.connectedDevices.has(deviceId)) {
+          this.logger.log(`Device ${deviceId} is already connected.`);
+          return;
+        }
+
         this.logger.log(
-          `[${manufacturerData}] Discovered device: ${peripheral.address} (\x1b[31m${peripheral.advertisement.localName || 'Unknown'}\x1b[32m)`,
+          `[${manufacturerData}] Discovered device: \x1b[31m${deviceId}`,
         );
         try {
           await this.connectToDevice(peripheral);
@@ -42,24 +49,24 @@ export class BluetoothService implements OnModuleInit {
     await this.setupBluetooth();
   }
 
-  private async disconnectFromDevice() {
-    try {
-      if (this.connectedDevice?.state === 'connected') {
-        this.logger.log(
-          `Disconnecting from \x1b[31m${this.connectedDevice.advertisement.localName || this.connectedDevice.address}\x1b[32m...`,
-        );
-        await this.connectedDevice.disconnectAsync();
-        await this.connectedDevice.removeAllListeners();
-        this.logger.warn(
-          `\x1b[34m${this.connectedDevice.advertisement.localName || this.connectedDevice.address} disconnected.`,
-        );
-      }
+  // private async disconnectFromDevice() {
+  //   try {
+  //     if (this.connectedDevice?.state === 'connected') {
+  //       this.logger.log(
+  //         `Disconnecting from \x1b[31m${this.connectedDevice.advertisement.localName || this.connectedDevice.address}\x1b[32m...`,
+  //       );
+  //       await this.connectedDevice.disconnectAsync();
+  //       await this.connectedDevice.removeAllListeners();
+  //       this.logger.warn(
+  //         `\x1b[34m${this.connectedDevice.advertisement.localName || this.connectedDevice.address} disconnected.`,
+  //       );
+  //     }
 
-      this.connectedDevice = null;
-    } catch (error) {
-      this.logger.error(`Error disconnecting from device: ${error.message}`);
-    }
-  }
+  //     this.connectedDevice = null;
+  //   } catch (error) {
+  //     this.logger.error(`Error disconnecting from device: ${error.message}`);
+  //   }
+  // }
 
   private async startScanning() {
     try {
@@ -104,81 +111,108 @@ export class BluetoothService implements OnModuleInit {
   }
 
   private async connectToDevice(peripheral: noble.Peripheral) {
-    this.logger.log(
-      `Connection to \x1b[31m${peripheral.advertisement.localName || peripheral.address}\x1b[32m...`,
-    );
-    await peripheral.connectAsync();
-    if (peripheral?.state === 'connected') {
+    const deviceId =
+      peripheral.advertisement.localName ||
+      peripheral.advertisement.manufacturerData?.toString('hex') ||
+      peripheral.address;
+
+    try {
       this.logger.log(
-        `\x1b[31m${peripheral.advertisement.localName || peripheral.address}\x1b[32m connected!`,
+        `Connection to \x1b[31m${peripheral.advertisement.localName || peripheral.address}\x1b[32m...`,
       );
-      this.connectedDevice = peripheral;
-
-      // setTimeout(async () => {
-      //   await this.disconnectFromDevice();
-      //   await this.startScanning();
-      // }, 20000);
-    } else {
-      this.logger.warn('Device is not connected.');
-    }
-
-    // Далі можна отримати сервіси та характеристики
-    const services = await peripheral.discoverServicesAsync([]);
-    console.log(services, 'services');
-    for (const service of services) {
-      const characteristics = await service.discoverCharacteristicsAsync([]);
-      this.logger.log(
-        `Service: ${service.uuid}, Features: ${characteristics.length}`,
-      );
-
-      for (const characteristic of characteristics) {
+      await peripheral.connectAsync();
+      if (peripheral?.state === 'connected') {
         this.logger.log(
-          `Characteristic: ${characteristic.uuid}, Properties: ${characteristic.properties.join(', ')}`,
+          `\x1b[31m${peripheral.advertisement.localName || peripheral.address}\x1b[32m connected!`,
+        );
+        this.connectedDevices.set(deviceId, peripheral);
+
+        // setTimeout(async () => {
+        //   await this.disconnectFromDevice();
+        //   await this.startScanning();
+        // }, 20000);
+      } else {
+        this.logger.warn('Device is not connected.');
+      }
+
+      // Далі можна отримати сервіси та характеристики
+      const services = await peripheral.discoverServicesAsync([]);
+      console.log(services, 'services');
+      for (const service of services) {
+        const characteristics = await service.discoverCharacteristicsAsync([]);
+        this.logger.log(
+          `Service: ${service.uuid}, Features: ${characteristics.length}`,
         );
 
-        if (characteristic.uuid === 'f000ffc0-0451-4000-b000-000000000000') {
-          if (characteristic.properties.includes('read')) {
-            const data = await characteristic.readAsync();
-            this.logger.log(
-              `ФФФФФФФФФФФФФФФФФФФФФФФФФФФФФФФФФ: ${data.toString('hex')}`,
-            );
-            const batteryLevel = data.readUInt8(0);
-            this.logger.log(`\x1b[0mФФФФФФФФФФФФФФФФФФФ: ${batteryLevel}%`);
-          }
-        }
-
-        // Читання рівня заряду батареї
-        if (service.uuid === '180f' && characteristic.uuid === '2a19') {
-          if (characteristic.properties.includes('read')) {
-            const data = await characteristic.readAsync();
-            this.logger.log(`Raw Battery Data: ${data.toString('hex')}`);
-            const batteryLevel = data.readUInt8(0);
-            this.logger.log(`\x1b[0mBattery Level: ${batteryLevel}%`);
-          }
-        }
-
-        // Якщо характеристика підтримує читання
-        if (characteristic.properties.includes('read')) {
-          const data = await characteristic.readAsync();
-          const buffer = Buffer.from('41540d0a', 'hex');
-          console.log(buffer.toString('utf8')); // Перетворення у текст
-
-
+        for (const characteristic of characteristics) {
           this.logger.log(
-            `Data from characteristic ${characteristic.uuid}: ${buffer.toString('utf8')}`,
+            `Characteristic: ${characteristic.uuid}, Properties: ${characteristic.properties.join(', ')}`,
           );
-        }
 
-        // Якщо характеристика підтримує підписку (notify)
-        if (characteristic.properties.includes('notify')) {
-          await characteristic.subscribeAsync();
-          characteristic.on('data', (data) => {
+          if (characteristic.uuid === 'f000ffc0-0451-4000-b000-000000000000') {
+            if (characteristic.properties.includes('read')) {
+              const data = await characteristic.readAsync();
+              this.logger.log(
+                `ФФФФФФФФФФФФФФФФФФФФФФФФФФФФФФФФФ: ${data.toString('hex')}`,
+              );
+              const batteryLevel = data.readUInt8(0);
+              this.logger.log(`\x1b[0mФФФФФФФФФФФФФФФФФФФ: ${batteryLevel}%`);
+            }
+          }
+
+          // Читання рівня заряду батареї
+          if (service.uuid === '180f' && characteristic.uuid === '2a19') {
+            if (characteristic.properties.includes('read')) {
+              const data = await characteristic.readAsync();
+              this.logger.log(`Raw Battery Data: ${data.toString('hex')}`);
+              const batteryLevel = data.readUInt8(0);
+              this.logger.log(`\x1b[0mBattery Level: ${batteryLevel}%`);
+            }
+          }
+
+          // Якщо характеристика підтримує читання
+          if (characteristic.properties.includes('read')) {
+            const data = await characteristic.readAsync();
+            const buffer = Buffer.from('41540d0a', 'hex');
+            console.log(buffer.toString('utf8')); // Перетворення у текст
+
+
             this.logger.log(
-              `Notification from ${characteristic.uuid}: ${data.toString('hex')}`,
+              `Data from characteristic ${characteristic.uuid}: ${buffer.toString('utf8')}`,
             );
-          });
+          }
+
+          // Якщо характеристика підтримує підписку (notify)
+          if (characteristic.properties.includes('notify')) {
+            await characteristic.subscribeAsync();
+            characteristic.on('data', (data) => {
+              this.logger.log(
+                `Notification from ${characteristic.uuid}: ${data.toString('hex')}`,
+              );
+            });
+          }
         }
       }
+    } catch (error) {
+      this.logger.error(`Error connecting to device ${deviceId}: ${error}`);
+      if (peripheral.state === 'connected') {
+        await peripheral.disconnectAsync();
+      }
     }
+  }
+
+  async disconnectAllDevices() {
+    this.logger.log('Disconnecting all devices...');
+    for (const [deviceId, peripheral] of this.connectedDevices.entries()) {
+      try {
+        if (peripheral.state === 'connected') {
+          await peripheral.disconnectAsync();
+          this.logger.log(`Disconnected device ${deviceId}.`);
+        }
+      } catch (error) {
+        this.logger.error(`Error disconnecting device ${deviceId}: ${error}`);
+      }
+    }
+    this.connectedDevices.clear();
   }
 }
