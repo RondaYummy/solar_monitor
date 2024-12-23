4d7ea1344a3739d6f48c039c61612cb30997bbee
RondoYummy <basrers@ukr.net>
2024-12-23 15:00:37 +0200

    2

--- src/bluetooth/bluetooth.service.ts
+++ src/bluetooth/bluetooth.service.ts
@@ -239,7 +239,7 @@ export class BluetoothService implements OnModuleInit {
 
   private getColorForRSSI(rssi: number): string {
     if (rssi >= -60) {
-      return '\x1b[32m'; // Зелений для сильного сигналу
+      return '\x1b[34m'; // Синій для сильного сигналу
     } else if (rssi >= -80) {
       return '\x1b[33m'; // Жовтий для середнього сигналу
     } else {
