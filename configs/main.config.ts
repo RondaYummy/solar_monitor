b32071c87920a2d4d36925c493b27e8f7defa18d
RondoYummy <basrers@ukr.net>
2024-12-25 21:22:27 +0200

    4

--- configs/main.config.ts
+++ configs/main.config.ts
@@ -1,6 +1,6 @@
 interface AllowedDevice {
   localName: string;
-  address: string;
+  address?: string;
 }
 
 interface Config {
@@ -10,16 +10,16 @@ interface Config {
 export const config: Config = {
   allowedDevices: [{
     localName: 'Andrii 1',
-    address: 'c8:47:80:12:9b:46',
+    // address: 'c8:47:80:12:9b:46',
   }, {
     localName: 'Andrii 2',
-    address: 'c8:47:80:21:bc:f4',
+    // address: 'c8:47:80:21:bc:f4',
   }, {
     localName: 'Andrii 3',
-    address: 'c8:47:80:12:41:99',
+    // address: 'c8:47:80:12:41:99',
   }, {
     localName: 'Andrii4',
-    address: 'c8:47:80:21:34:82',
+    // address: 'c8:47:80:21:34:82',
   }
   ],
 };
