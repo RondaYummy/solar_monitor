5608041fc52a0251d95db994445eae448051efe7
RondoYummy <basrers@ukr.net>
2024-12-26 23:53:25 +0200

    6

--- configs/main.config.ts
+++ configs/main.config.ts
@@ -10,16 +10,16 @@ interface Config {
 export const config: Config = {
   allowedDevices: [{
     localName: 'Andrii 1',
-    address: 'c8:47:80:12:9b:46',
+    // address: 'c8:47:80:12:9b:46',
   }, {
     localName: 'Andrii 2',
-    address: 'c8:47:80:21:bc:f4',
+    // address: 'c8:47:80:21:bc:f4',
   }, {
     localName: 'Andrii 3',
-    address: 'c8:47:80:12:41:99',
+    // address: 'c8:47:80:12:41:99',
   }, {
     localName: 'Andrii4',
-    address: 'c8:47:80:21:34:82',
+    // address: 'c8:47:80:21:34:82',
   }
   ],
 };
