225622c4c426b8988e391e80555203a2e31392f0
RondoYummy <basrers@ukr.net>
2025-01-12 22:13:40 +0200

    2222

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { BluetoothService } from './bluetooth/bluetooth.service';
-// import { BluetoothService } from './bluetooth/dbuss.service';
+// import { BluetoothService } from './bluetooth/bluetooth.service';
+import { BluetoothService } from './bluetooth/dbuss.service';
 import { TelegramModule } from './telegram/telegram.module';
 import { validate } from './env.validation';
 import { ConfigModule } from '@nestjs/config';
